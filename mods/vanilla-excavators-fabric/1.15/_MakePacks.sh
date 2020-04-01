23c85a190d2b3a3380cf5ed8ad6301f188aaf6de
MelanX <meissi97@me.com>
2020-04-01 19:49:07 +0200

    deleted 1.15 from fabric excavators because.. theres no 1.15 version

--- mods/vanilla-excavators-fabric/1.15/_MakePacks.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd -- "$(dirname "$0")"
-printf "Generating size packs + zipping up...\n"
-
-# Run zip task
-npm run makezips
-
-printf "\nComplete!\n"
-read -p "Press [Enter] to close."
